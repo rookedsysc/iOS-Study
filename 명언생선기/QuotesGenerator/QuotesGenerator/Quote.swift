a649976667ac739c6baf19bed5ca28ae314ba031
rookedsysc <rookedsysc@Rookedsyscs-MacBook-Air.local>
2022-08-18 00:38:02 +0900

    명언생선기 끝

---
+++ 명언생선기/QuotesGenerator/QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by Rookedsysc on 2022. 8. 18..
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
